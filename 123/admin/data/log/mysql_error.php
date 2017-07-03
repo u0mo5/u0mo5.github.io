3856c39f9efeed1522821dcfbc2b6862310fb23c
u0mo5 <u0mo5@qq.com>
2017-07-03 11:30:46 +0800

    del log

--- 123/admin/data/log/mysql_error.php
+++
@@ -1,6 +0,0 @@
-<?die;?>||/feedback/index.php?|初始化|2002Connection refused|1496351180|183.57.53.196|NO_POSTDATA|
-<?die;?>||/admin/index.php?|初始化|2002Connection refused|1496650412|113.232.107.146|NO_POSTDATA|
-<?die;?>||/admin/index.php?|初始化|2002Connection refused|1496650422|101.226.65.102|NO_POSTDATA|
-<?die;?>||/admin/index.php?|初始化|1049Unknown database '114la'|1496890075|113.232.117.208|NO_POSTDATA|
-<?die;?>||/admin/index.php?|初始化|1049Unknown database '114la'|1496890076|113.232.117.208|NO_POSTDATA|
-<?die;?>||/admin/index.php?|初始化|1045Access denied for user 'root'@'localhost' (using password: YES)|1498792036|127.0.0.1|NO_POSTDATA|
